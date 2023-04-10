2206d2b2a443b969f5e439ac7ebb03b7a7e4fc68
Romain86 <rmartinet77@gmail.com>
2023-04-10 15:39:54 +0200

    Création du composant de recherche
    Création du composant de recherche pour une ville de minimum 50000 habitants

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c6789917d2msh6d605d99d8ef459p17517bjsn6abf6b106e20',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
